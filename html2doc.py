7d829df911e2b8ca923ef43adaceaf56181d9d32
CF <lawchunfu@gmail.com>
2020-03-09 00:33:54 +0800

    1) Added words to useless_words list 2) Added some comments 3) Clean up some useless commented out old functions

--- html2doc.py
+++ html2doc.py
@@ -20,10 +20,12 @@ def sorted_alphanumeric(data):
 document = Document()
 firstPagebreak = True  # set flag for pagebreak
 
+# formatting for whole document
 section = document.sections[0]
 section.left_margin = Cm(2.25)
 section.top_margin = Cm(2.0)
 
+# formatting for heading of the chapter
 styles = document.styles
 new_heading_style = styles.add_style('New Heading', WD_STYLE_TYPE.PARAGRAPH)
 new_heading_style.base_style = styles['Heading 1']
@@ -40,28 +42,33 @@ for filename in sorted_alphanumeric(os.listdir(directory)):
         extName = os.path.join(directory, filename)
 
         # if extracting from local html
-        path = 'file:///'+extName
+        path = 'file:///' + extName
         filename = Path(path).stem
         html = urlopen(path)
-
-        # if scrape from web
-        # html = urlopen('https://www.biqupa.com/7_7817/6864689.html')
-
         content = html.read().decode('gb2312', 'ignore')
-        # clean up before adding into word doc
-        regex1 = r"^(.*?)<div id=\"content\" class=\"showtxt\">"
-        regex2 = r'(?<=</div>).*'
-        regex3 = r'^(.*?)章'
+
+        # clean up html in preparations for processing
+        regex1 = '^(.*?)<div id=\"content\" class=\"showtxt\">'
+        regex2 = '(?<=</div>).*'
+        regex3 = '^(.*?)章'
         regex4 = '<br />\r<br />&nbsp;'
         replace_txt4 = '<br />&nbsp;'
 
-        cleaned_txt = re.sub(regex1, '', content, flags=re.DOTALL).strip()
-        cleaned_txt = re.sub(regex2, '', cleaned_txt, flags=re.DOTALL).strip()
-        cleaned_txt = re.sub(regex3, '', cleaned_txt, flags=re.DOTALL).strip()
+        cleaned_txt = re.sub(regex1, '', content, flags=re.DOTALL)
+        cleaned_txt = re.sub(regex2, '', cleaned_txt, flags=re.DOTALL)
+        cleaned_txt = re.sub(regex3, '', cleaned_txt, flags=re.DOTALL)
         cleaned_txt = cleaned_txt.replace(regex4, replace_txt4)
 
         # remove nonsense words
-        useless_words = ["更新最快", "手机端一秒住槟提供精彩\\小fx。", "首发", "78中文首发"]
+        useless_words = [
+            "更新最快",
+            "手机端一秒住槟提供精彩\\小fx。",
+            "首发",
+            "78中文首发",
+            "叶辰萧初然来源：",
+            "叶辰萧初然"
+            ]
+
         for index, word in enumerate(useless_words):
             cleaned_txt = cleaned_txt.replace(useless_words[index], "")
 
@@ -73,38 +80,18 @@ for filename in sorted_alphanumeric(os.listdir(directory)):
             pagebreak = document.add_paragraph().add_run()
             pagebreak.add_break(WD_BREAK.PAGE)
 
-        # if scrape from web or local html from http://blog.weimengclass.com/
-        # unfinished code - to change to individual methods
-
-        # text = soup.find('div', attrs={'class': 'entry-content'}).get_text()
-        # content_split = re.split(r'第(\d+)章', text)
-        # print(content_split[39])
-
-        # for x in range(len(content_split)):
-            # print(x)
-
-        # header = content_split[0].strip()+'章'
-        # heading = document.add_paragraph(header, style='New Heading')
-
-        # if extracting from local html
+        # Add heading to the top of the page, using filename of HTML
         heading = document.add_paragraph(filename, style='New Heading')
 
-        # formatting
+        # formatting for content
         heading.paragraph_format.space_before = Pt(0)
-
         paragraph = document.add_paragraph()
         paragraph.paragraph_format.line_spacing = 1.5
 
-        # if scrape from web
-        # run = paragraph.add_run(content_split[1])
-
         # if extracting from local html
-        # add into word doc
+        # add the cleaned up content into word doc
         run = paragraph.add_run(soup.get_text(separator='\n'))
-        # run = paragraph.add_run(cleaned_txt)
+        run.font.name = 'Arial'
+        run.font.size = Pt(10)
 
-        font = run.font
-        font.name = 'Arial'
-        font.size = Pt(10)
-
-document.save('D:\\reading\\yechen\\raw\\combined.docx')
+document.save(directory + 'combined.docx')
